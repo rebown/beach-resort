8b7c0cfe40e426d7bd781ad2e0926a756816f7bb
rebown <kiril270405@gmail.com>
2020-03-27 23:25:18 +0200

    Room list component completed

--- resort/src/components/RoomList.jsx
+++ resort/src/components/RoomList.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Room } from './Room'
 
-export default function RoomList() {
+export default function RoomList({ rooms }) {
+    debugger
+    if(rooms.length === 0) {
+         return (
+             <div className="empty-search">
+                 <h3>Unfortunately no rooms matched your search parameters</h3>
+             </div>
+         )
+    }
+
     return (
-        <div>
-            room list
-        </div>
+        <section className="roomslist">
+            <div className="roomslist-center">
+                {
+                    rooms.map(room => {
+                        return <Room key={room.id} room={room} />
+                    })
+                }
+            </div>
+        </section>
     )
 }
