1becb83b254671e2351873faa343e1a310db6446
rebown <kiril270405@gmail.com>
2020-03-27 23:24:51 +0200

    hoc for Consumer wrapper

--- resort/src/context.jsx
+++ resort/src/context.jsx
@@ -48,4 +48,14 @@ class RoomProvider extends React.Component {
 
 const RoomConsumer = RoomContext.Consumer;
 
+export function withRoomConsumer(Component) {
+    return function ConsumerWrapper(props) {
+        return (
+            <RoomConsumer>
+                {value => <Component {...props} context={value} />}
+            </RoomConsumer>
+        )
+    }
+}
+
 export { RoomProvider, RoomConsumer, RoomContext };
