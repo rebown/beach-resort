0fecb370098cc9f0e530b93606ae90b86e993e1c
rebown <kiril270405@gmail.com>
2020-03-27 22:52:16 +0200

    Rooms page filter and list components

---
+++ resort/src/components/RoomContainer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import RoomFilter from './RoomFilter'
+import RoomList from './RoomList'
+import { RoomConsumer } from '../context'
+
+
+
+// export default function RoomContainer() {
+//     return (
+//        <RoomConsumer>
+//            {
+//                ({ loading, sortedRooms, rooms }) => {
+//                     return (
+//                         <div>
+//                             <RoomFilter/>
+//                             <RoomList/>
+//                         </div>
+//                     )
+//                }
+//            }
+//        </RoomConsumer>
+//     )
+// }
