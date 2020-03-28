f152a8bceef5689a9cdfd4a73075a2d3eebf70c1
rebown <kiril270405@gmail.com>
2020-03-28 20:49:03 +0200

    room container completed

--- resort/src/components/RoomContainer.jsx
+++ resort/src/components/RoomContainer.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import RoomFilter from './RoomFilter'
 import RoomList from './RoomList'
-import { RoomConsumer } from '../context'
+import { Loading } from './Loading'
+import { withRoomConsumer } from '../context'
 
+function RoomContainer({ context }) {
+    const { loading, sortedRooms, rooms } = context;
 
+    if(loading) {
+        return <Loading/>
+    }
+    return (
+        <div>
+            <RoomFilter rooms={rooms} />
+            <RoomList rooms={sortedRooms}/>
+        </div>
+    )
+}
+
+export default withRoomConsumer(RoomContainer)
 
 // export default function RoomContainer() {
 //     return (
